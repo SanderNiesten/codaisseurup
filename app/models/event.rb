737d45621cd2e7a16f901617f560cdaee3a9ebb6
SanderNiesten <sanderniesten@live.nl>
2017-10-10 17:49:28 +0200

    One-to-Many: Controllers

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :description, presence: true, length { maximum: 500 }
+  validates :description, presence: true, length: { maximum: 500 }
 end

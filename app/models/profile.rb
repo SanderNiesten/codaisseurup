ccafd56338ee58f458a0360fbd32d95e6dc5b6ef
SanderNiesten <sanderniesten@live.nl>
2017-10-11 17:41:25 +0200

    Add Models Specs: Rooms

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,4 @@
 class Profile < ApplicationRecord
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :bio, presence: true
@@ -8,4 +7,7 @@ class Profile < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def self.by_letter(letter)
+    where("first_name LIKE ?", "#{letter}%").order(:first_name)
+  end
 end

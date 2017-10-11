d57c63ab08006ad724ad6f7eaf93317e60207c6e
SanderNiesten <sanderniesten@live.nl>
2017-10-11 13:01:06 +0200

    Add navbar specs

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,15 +1,15 @@
 FactoryGirl.define do
   factory :event do
-    name              "Party"
-    description       "Surprise for someone"
+    name              { Faker::Lorem.word }
+    description       { Faker::Lorem.sentence(40) }
     location          { Faker::Address.city }
     includes_food     false
     includes_drinks   true
-    price             5
+    price             { Faker::Commerce.price }
     starts_at         10.days.from_now
     ends_at           12.days.from_now
     capacity          20
-    active            true
+    
     user              { build(:user) }
 
     trait :active do

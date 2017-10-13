0ee4e92b9bfb6198af1062d71d7e22da97a9866a
SanderNiesten <sanderniesten@live.nl>
2017-10-13 11:36:36 +0200

    fix typos in Registration Controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,9 @@ class RegistrationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @registrations = current_user.registrations.create(registration_params)
+    @registration = current_user.registrations.create(registration_params)
 
-    redirect_to @registration.event, notice: "Thank you for regeristing!"
+    redirect_to @registration.event, notice: "Thank you for registering!"
   end
 
   private
